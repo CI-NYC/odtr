aeb69f89ffcaf4aad289d5b1c9bf31e624c7da39
Nick Williams <ntwilliams.personal@gmail.com>
2022-03-22 10:14:15 -0700

    modifications.

--- R/folds.R
+++
@@ -1,7 +0,0 @@
-make_folds <- function(data, V) {
-    folds <- origami::make_folds(data, V = V)
-    if (V == 1) {
-        folds[[1]]$training_set <- folds[[1]]$validation_set
-    }
-    folds
-}
--- R/utils.R
+++
@@ -1,1 +0,0 @@
-g <- glue::glue
